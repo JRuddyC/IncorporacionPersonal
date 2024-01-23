cd0d77c91b007b454b8f7b8e648b03d0bde047e5
L-Hidalgo <l0hidalgo001698@gmail.com>
2024-01-23 07:45:42 -0400

    DataTable editable row por completar crud

--- resources/js/Pages/Incorporaciones/Index.vue
+++ resources/js/Pages/Incorporaciones/Index.vue
@@ -1,33 +1,64 @@
 <template>
-    <Head title="Migraciones"></Head>
+    <Head title="Incorporaciones"></Head>
     <div class="grid">
         <div class="col-12">
             <div class="card">
                 <Toolbar class="mb-4">
                     <template v-slot:start>
-                        <h5>MIGRACIONES</h5>
+                        <h5>INCORPORACIONES</h5>
                     </template>
                     <template v-slot:end>
-                        <div class="my-2">
-                            <Button
-                                label="IMPORTAR"
-                                icon="pi pi-download"
-                                class="p-button-success mr-2"
-                                @click="openModalImport()"
-                            />
-                        </div>
+                        <div class="my-2"></div>
                     </template>
                 </Toolbar>
-                asd asd asd sa
+                <DataTable />
+            </div>
+            <div class="card">
+                <DataTable
+                    v-model:editingRows="editingRows"
+                    :value="products"
+                    editMode="row"
+                    dataKey="id"
+                    @row-edit-save="onRowEditSave"
+                    :pt="{
+                        table: { style: 'min-width: 50rem' },
+                        column: {
+                            bodycell: ({ state }) => ({
+                                style:
+                                    state['d_editing'] &&
+                                    'padding-top: 0.6rem; padding-bottom: 0.6rem',
+                            }),
+                        },
+                    }"
+                >
+                    <Column field="item" header="ITEM" style="width: 20%">
+                        <template #editor="{ data, field }">
+                            <InputText v-model="data[field]" />
+                        </template>
+                    </Column>
+                    <Column field="gerencia" header="GERENCIA" style="width: 20%">
+                        <template #editor="{ data, field }">
+                            <InputText v-model="data[field]" />
+                        </template>
+                    </Column>
+                    <Column field="departameto" header="DEPARTAMENTO" style="width: 20%">
+                        <template #editor="{ data, field }">
+                            <InputText v-model="data[field]" />
+                        </template>
+                    </Column>
+                    <Column field="departameto" header="DEPARTAMENTO" style="width: 20%">
+                        <template #editor="{ data, field }">
+                            <InputText v-model="data[field]" />
+                        </template>
+                    </Column>
+                </DataTable>
             </div>
         </div>
-        <ImportarModal ref="refImportarModal" />
     </div>
 </template>
 
 <script>
 import AppLayout from "../../Layouts/AppLayout.vue";
-import Button from "primevue/button";
 export default {
     name: "Index",
     layout: AppLayout,
@@ -36,15 +67,23 @@ export default {
 
 <script setup>
 import { ref } from "vue";
-import ImportarModal from "./Components/ImportarModal.vue";
 import { Head } from "@inertiajs/inertia-vue3";
 import { Inertia } from "@inertiajs/inertia";
 import Toolbar from "primevue/toolbar";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row"; // optional
+import Button from "primevue/button";
 
-const refImportarModal = ref();
-function openModalImport() {
-    refImportarModal.value?.open();
-}
+const products = ref();
+const editingRows = ref([]);
+
+const onRowEditSave = (event) => {
+    let { newData, index } = event;
+
+    products.value[index] = newData;
+};
 </script>
 
 <style lang="scss" scoped></style>
